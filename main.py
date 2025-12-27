5beb60f9361d6e758011a0dd16555433c931c1e3
reallemon <spencerpenrod@gmail.com>
2025-12-26 19:28:22 -0700

    feat: add reroll button

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import json
 import random
 
 def main(): 
-    st.title('Coding Mystery Box')
+    st.set_page_config(page_title='Coding Mystery Box')
     
     if 'random' not in st.session_state:
         st.session_state['random'] = []
+    
+    if 'data' not in st.session_state:
+        st.session_state['data'] = {}
     
     col_1, col_2 = st.columns(2)
     with col_1:
@@ -19,17 +22,48 @@ def main():
         for i in range(len(st.session_state.random) -1, -1, -1):
             option = st.session_state.random[i]
             
-            st.button('🗑️', key=f'delete_{i}', on_click=delete_index, args=(i,))
             
-            st.write('## Input')
-            st.write(f"{option['input']['name']}: {option['input']['description']}")
-            
-            st.write('## Constraint')
-            st.write(f"{option['constraint']['name']}: {option['constraint']['description']}")
-            
-            st.write('## Output')
-            st.write(f"{option['output']['name']}: {option['output']['description']}")
-            st.write('---')
+            with st.container(border=True):
+                d_col_1, d_col_2 = st.columns([6,1])
+                
+                with d_col_2:
+                    st.button('🗑️', key=f'delete_{i}', on_click=delete_index, args=(i,))
+                
+                c1, c2 = st.columns([1, 5])
+                
+                with c1:
+                    st.write("")
+                    st.write("")
+                    st.button('🔄', key=f'reroll_inputs_{i}', on_click=reroll_idea, args=(i, 'inputs',))
+                
+                with c2:
+                    st.write('## Input')
+                    st.write(f"{option['inputs']['name']}: {option['inputs']['description']}")
+                
+                c1, c2 = st.columns([1, 5])
+                
+                with c1:
+                    st.write("")
+                    st.write("")
+                    st.button('🔄', key=f'reroll_constraints_{i}', on_click=reroll_idea, args=(i, 'constraints',))
+                
+                with c2:
+                    st.write('## Constraint')
+                    st.write(f"{option['constraints']['name']}: {option['constraints']['description']}")
+                    
+                c1, c2 = st.columns([1, 5])
+                
+                with c1:
+                    st.write("")
+                    st.write("")
+                    st.button('🔄', key=f'reroll_outputs_{i}', on_click=reroll_idea, args=(i, 'outputs',))
+                
+                with c2:
+                    st.write('## Output')
+                    st.write(f"{option['outputs']['name']}: {option['outputs']['description']}")
+
+def reroll_idea(index, category_key):
+    st.session_state.random[index][category_key] = random.choice(st.session_state['data'][category_key])
 
 def delete_index(index):
     del st.session_state.random[index]
@@ -39,27 +73,38 @@ def clear_session_state():
         st.session_state['random'] = []
 
 def add_random_choice():
-    try:
-        with open('data.json', 'r') as file:
-            data = json.load(file)
-        # List of dicts
-        inputs = data['tables']['fabric_inputs']
-        constraints = data['tables']['logic_constraints']
-        outputs = data['tables']['output_mediums']
-        
-        rand_input = random.choice(inputs)
-        rand_constraint = random.choice(constraints)
-        rand_output = random.choice(outputs)
-        
-        rand_choice = {
-            "input": rand_input,
-            "constraint": rand_constraint,
-            "output": rand_output
-        }
-        
-        st.session_state.random.append(rand_choice)
-    except:
-        print("Something went wrong")
+    data = st.session_state['data']
+            
+    rand_input = random.choice(data['inputs'])
+    rand_constraint = random.choice(data['constraints'])
+    rand_output = random.choice(data['outputs'])
+    
+    rand_choice = {
+        "inputs": rand_input,
+        "constraints": rand_constraint,
+        "outputs": rand_output
+    }
+    
+    st.session_state.random.append(rand_choice)
         
 if __name__ == "__main__":
+    try:
+        with open('data.json', 'r') as file:
+            raw_data = json.load(file)
+        # List of dicts
+        inputs = raw_data['tables']['fabric_inputs']
+        constraints = raw_data['tables']['logic_constraints']
+        outputs = raw_data['tables']['output_mediums']
+        
+        data = {
+            "inputs": inputs,
+            "constraints": constraints,
+            "outputs": outputs
+        }
+        
+        st.session_state['data'] = data
+        
+    except:
+        print("Something went wrong")
+    
     main()
